4062355a3640e559e7eff6f7975d7aafd05088a8
Mohamed Nijadi <mohammednijadi47@gmail.com>
2023-09-30 14:46:23 +0100

    Add utils for data  generation and preprocessing

---
+++ utils.py
@@ -0,0 +1,153 @@
+import numpy as np
+from faker import Faker
+import random
+from tqdm import tqdm
+from babel.dates import format_date
+from tensorflow.keras.utils import to_categorical
+import tensorflow.keras.backend as K
+from tensorflow.keras.models import Model
+import matplotlib.pyplot as plt
+
+fake = Faker()
+Faker.seed(12345)
+random.seed(12345)
+
+# format of the data we would like to generate
+FORMATS = ['short',
+           'medium',
+           'long',
+           'full',
+           'full',
+           'full',
+           'full',
+           'full',
+           'full',
+           'full',
+           'full',
+           'full',
+           'full',
+           'd MMM YYY', 
+           'd MMMM YYY',
+           'dd MMM YYY',
+           'd MMM, YYY',
+           'd MMMM, YYY',
+           'dd, MMM YYY',
+           'd MM YY',
+           'd MMMM YYY',
+           'MMMM d YYY',
+           'MMMM d, YYY',
+           'dd.MM.YY']
+
+LOCALES = ['en_US']
+
+def load_date():
+    '''
+        loads fake dates
+    '''
+    dt = fake.date_object()
+
+    try:
+        human_readable = format_date(dt, format=random.choice(FORMATS), locale='en_US')
+        human_readable = human_readable.lower()
+        human_readable = human_readable.replace(',','')
+        machine_readable = dt.isoformat()
+        
+    except AttributeError as e:
+        return None, None, None
+
+    return human_readable, machine_readable, dt
+
+def load_dataset(m):
+    '''
+        loads a dataset with m examples and vocabularies
+    '''
+    
+    human_vocab = set()
+    machine_vocab = set()
+    dataset = []
+    Tx = 30
+
+    for i in tqdm(range(m)):
+        h, m, _ = load_date()
+        if h is not None:
+            dataset.append((h, m))
+            human_vocab.update(tuple(h))
+            machine_vocab.update(tuple(m))
+    
+    human = dict(zip(sorted(human_vocab) + ['<unk>', '<pad>'], 
+                     list(range(len(human_vocab) + 2))))
+    inv_machine = dict(enumerate(sorted(machine_vocab)))
+    machine = {v:k for k,v in inv_machine.items()}
+ 
+    return dataset, human, machine, inv_machine
+
+def preprocess_data(dataset, human_vocab, machine_vocab, Tx, Ty):
+    
+    X, Y = zip(*dataset)
+    
+    X = np.array([string_to_int(i, Tx, human_vocab) for i in X])
+    Y = [string_to_int(t, Ty, machine_vocab) for t in Y]
+    
+    Xoh = np.array(list(map(lambda x: to_categorical(x, num_classes=len(human_vocab)), X)))
+    Yoh = np.array(list(map(lambda x: to_categorical(x, num_classes=len(machine_vocab)), Y)))
+
+    return X, np.array(Y), Xoh, Yoh
+
+def string_to_int(string, length, vocab):
+    '''
+    converts strings in the vocabulary into a list of integers representing the positions of the
+    input string's characters in the "vocab"
+    '''
+    
+    # lower to standardize
+    string = string.lower()
+    string = string.replace(',','')
+    
+    if len(string) > length:
+        string = string[:length]
+        
+    rep = list(map(lambda x: vocab.get(x, '<unk>'), string))
+    
+    if len(string) < length:
+        rep += [vocab['<pad>']] * (length - len(string))
+    
+    # print (rep)
+    return rep
+
+
+def int_to_string(ints, inv_vocab):
+    '''
+    output a machine readable list of characters based on a list of indexes in the machine's vocabulary
+    '''
+    
+    l = [inv_vocab[i] for i in ints]
+    return l
+
+
+EXAMPLES = ['3 May 1979', '5 Apr 09', '20th February 2016', 'Wed 10 Jul 2007']
+
+def run_example(model, input_vocabulary, inv_output_vocabulary, text):
+    encoded = string_to_int(text, TIME_STEPS, input_vocabulary)
+    prediction = model.predict(np.array([encoded]))
+    prediction = np.argmax(prediction[0], axis=-1)
+    return int_to_string(prediction, inv_output_vocabulary)
+
+def run_examples(model, input_vocabulary, inv_output_vocabulary, examples=EXAMPLES):
+    predicted = []
+    for example in examples:
+        predicted.append(''.join(run_example(model, input_vocabulary, inv_output_vocabulary, example)))
+        print('input:', example)
+        print('output:', predicted[-1])
+    return predicted
+
+
+def softmax(x, axis=1):
+    ndim = K.ndim(x)
+    if ndim == 2:
+        return K.softmax(x)
+    elif ndim > 2:
+        e = K.exp(x - K.max(x, axis=axis, keepdims=True))
+        s = K.sum(e, axis=axis, keepdims=True)
+        return e / s
+    else:
+        raise ValueError('Cannot apply softmax to a tensor that is 1D')
